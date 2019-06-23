27aa08610f371dbae32d17602e3890ef7f042bc4
Ashish Patil <ashishnitinpatil@gmail.com>
2019-06-23 04:31:42 +0400

    ~use Arkane provider for ropsten network

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,9 +1,22 @@
+require('dotenv').config();
+const ArkaneProvider = require("@arkane-network/truffle-arkane-provider");
+
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
-    }
+    },
+    ropsten: {
+      provider: () =>
+        new ArkaneProvider({
+            apiKey: process.env["ARKANE_API_KEY"],
+            baseUrl: 'https://api.arkane.network',
+            providerUrl: 'https://ropsten.infura.io'
+        }),
+      network_id: '3',
+    },
   }
 };
